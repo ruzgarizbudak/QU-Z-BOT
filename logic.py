f12291175e3372840d87dd1ca2956cdafbff44c0
ruzgarizbudak <izbudakruzgar@gmail.com>
2025-10-11 21:12:55 +0300

    mesj

--- logic.py
+++ logic.py
@@ -1,4 +1,4 @@
-# Görev 2 - İhtiyacınız olan her şeyi içe aktarın
+from discord import ui,ButtonStyle
 
 class Question:
     def __init__(self, text, answer_id, *options):
@@ -11,13 +11,19 @@ class Question:
         return self.__text 
 
     def gen_buttons(self):
-        # Görev 3 - Dahili klavyeyi oluşturmak için bir metot oluşturun
-        return buttons
+        butonlar= []
+        for indeks, cevap in enumerate(self.options):
+            if indeks == self.__answer_id:
+                butonlar.append(ui.Button(label= cevap, style=ButtonStyle.primary, custom_id=f'correct_{indeks}'))
+            else:
+                butonlar.append(ui.Button(label= cevap, style=ButtonStyle.primary, custom_id=f'wrong_{indeks}'))
+        return butonlar
 
 # Görev 4 - Listeyi sorularınızla doldurun
 quiz_questions = [
    Question("Kediler onları kimse görmediğinde ne yapar?", 1, "Uyurlar", "Espri yazarlar"),
    Question("Kediler sevgilerini nasıl ifade ederler?", 0, "Yüksek sesle mırıldanırlar", "Sevimli fotoğraflar", "Havlar"),
-   Question("Kediler hangi kitapları okumayı sever?", 3, "Kişisel gelişim kitapları", "Zaman yönetimi: Günde 18 saat nasıl uyunur","Sahibinizden 5 dakika erken uyumanın 101 yolu", "İnsan yönetimi rehberi")
-]
+   Question("Kediler hangi kitapları okumayı sever?", 3, "Kişisel gelişim kitapları", "Zaman yönetimi: Günde 18 saat nasıl uyunur","Sahibinizden 5 dakika erken uyumanın 101 yolu", "İnsan yönetimi rehberi"),
+   Question("Kediler nereye tuvalet yapar?", 0, "Kumlarına", "Klozet","Sahibinin Yatağına", "Balkona")
+   ]
 
